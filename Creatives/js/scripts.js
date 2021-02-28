9c8ddb87a1989bb3e32562304e8cd9898bcef4c6
Hekkijer <73902801+Hekkijer@users.noreply.github.com>
2021-02-28 14:20:15 +0200

    Add files via upload

---
+++ Creatives/js/scripts.js
@@ -0,0 +1,8 @@
+// Google Maps
+function myMap() {
+var mapProp= {
+  center:new google.maps.LatLng(51.508742,-0.120850),
+  zoom:15,
+};
+var map = new google.maps.Map(document.getElementById("map"),mapProp);
+}
